a581c8bb8d095431dfd49f71a8ee437282068b6b
Joel Ruben Cauti Rojas <joel.cauti@gmail.com>
2017-11-04 19:32:14 -0500

    nueva actualizacion

--- app/src/main/java/com/android/ejemplologin/MainActivity.java
+++ app/src/main/java/com/android/ejemplologin/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.ejemplologin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,36 +26,41 @@ public class MainActivity extends AppCompatActivity {
         user = (EditText) findViewById(R.id.editText);
         pass = (EditText) findViewById(R.id.editText2);
         boton = (Button) findViewById(R.id.button);
-        //Aqui empieza el cliente http
-        AsyncHttpClient cliente = new AsyncHttpClient();
-        RequestParams datos = new RequestParams();
-        datos.put("usuario",user.getText().toString());
-        datos.put("clave",pass.getText().toString());
-        cliente.post(getApplicationContext(), base_url, datos, new AsyncHttpResponseHandler() {
+        boton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                if(statusCode == 200)
-                {
-                    String respuesta = new String(responseBody);
-                    if("correcto".equals(respuesta))
-                    {
-                        startActivity(new Intent(MainActivity.this,Main2Activity.class));
-                        finish();
+            public void onClick(View v) {
+                //Aqui empieza el cliente http
+                AsyncHttpClient cliente = new AsyncHttpClient();
+                RequestParams datos = new RequestParams();
+                datos.put("usuario",user.getText().toString());
+                datos.put("clave",pass.getText().toString());
+                cliente.post(getApplicationContext(), base_url, datos, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        if(statusCode == 200)
+                        {
+                            String respuesta = new String(responseBody);
+                            if("correcto".equals(respuesta))
+                            {
+                                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+                                finish();
+                            }
+                            else if("incorrecto".equals(respuesta))
+                            {
+                                Toast.makeText(getApplicationContext(),"Usuario y/o contraseña incorrecto",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(),"Error en el servidor",Toast.LENGTH_LONG).show();
+                        }
                     }
-                    else if("incorrecto".equals(respuesta))
-                    {
-                        Toast.makeText(getApplicationContext(),"Usuario y/o contraseña incorrecto",Toast.LENGTH_LONG).show();
-                    }
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(),"Error en el servidor",Toast.LENGTH_LONG).show();
-                }
-            }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                Toast.makeText(getApplicationContext(),"Sin conexion",Toast.LENGTH_LONG).show();
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        Toast.makeText(getApplicationContext(),"Sin conexion",Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
